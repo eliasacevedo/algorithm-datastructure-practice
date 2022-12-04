ae4414513b84138c0f3534060262ec37739764b1
Elias Acevedo <eliasjoseacevedo61@gmail.com>
2022-12-03 22:34:18 -0400

    added weird task instructions

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Algorithms and Data structure practice
+
+This repo contains exercices what I done just for practice
---
+++ weird-task/exercice.js
@@ -0,0 +1,4 @@
+// From a text-string sentence, 
+// 1. inverse all words with even number of characters, 
+// 2. remove duplicate words with uneven number of characters and 
+// 3. return as fast as possible the length-sorted words as a new string sentence
